5b535de7c1dcedadae987af5b57f9b26937c349c
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-09-17 11:24:39 +0600

    home

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+const { urlencoded } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const port = 5000;
 const app = express();
+app.set("view engine", "ejs");
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(urlencoded({ extends: false }));
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hi this is Mahesh Biswas",
+  });
+});
+mongoose
+  .connect("mongodb://localhost:27017/test")
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is listen on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`This is a error`);
+  });
