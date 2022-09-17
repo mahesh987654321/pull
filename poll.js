dbdca53fbd94e700fe92199de43b91d633beab56
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-09-17 11:39:08 +0600

    user_schema

---
+++ poll.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+const pollSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  totalVote: {
+    type: Number,
+    default: 0,
+  },
+  options: {
+    type: [
+      {
+        name: String,
+        vote: Number,
+      },
+    ],
+  },
+});
+const Poll = model("poll", pollSchema);
+module.exports = Poll;
